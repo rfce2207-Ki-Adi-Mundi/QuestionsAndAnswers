b73a7f14b493b49322e4c4649bb2fd2490dd1400
Alex Lee <alexylee15@gmail.com>
2022-09-15 20:59:34 -0400

    writing correct get request to qa/questions

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-const client = require('./db/index.js');
+const db = require('./db/index.js');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 let app = express();
 module.exports.app = app;
 
 const port = process.env.PORT || 8080;
 
+//MIDDLEWARE
+app.use(cors());
+app.use(morgan('dev'));
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
+})
+
+app.get('/qa/questions', (req, res) => { //IT MAY BE FASTER TO MAKE PRODUCT_ID INTO VARCHAR IN TABLE SCHEMA
+  let product = parseInt(req.query.product_id);
+  let page = parseInt(req.query.page) || 1;
+  let limit = parseInt(req.query.count) || 10;
+  let info = {
+    product_info: product,
+  };
+  if (limit > 50) {
+    limit = 50;
+  }; //SELECT ONLY INFO YOU NEED, THEN ADD ANOTHRE QUERY TO ADD ANSWERS TO INFO OBJECT
+  db.query(`SELECT * from questions where product_id = ${product} order by questions.helpful desc limit ${limit} offset ${(page - 1) * 10};`, (err, result) => {
+    if (err) {
+      res.sendStatus(400);
+      console.log(err);
+    }
+    info.results = result.rows
+    res.status(200).json(info);
+    console.log(info);
+  });
 })
--- db/index.js
+++ db/index.js
@@ -15,14 +15,32 @@ client.connect((err) => {
       console.error('error connecting: ' + err.stack);
       return;
     }
-    console.log('connected as id ' + client.user);
+    console.log('connected to ' + client.database + ' as id ' + client.user);
   });
 
-client.query(`Select * from test`, (err, res) => {
-  if (!err) {
-    console.log(res.rows);
-  } else {
-    console.log(err.message);
+
+client.query(`CREATE TABLE IF NOT EXISTS questions ( question_id int PRIMARY KEY NOT NULL, product_id int NOT NULL, body text NOT NULL, date_written bigint NOT NULL, asker_name varchar(40) NOT NULL, asker_email text NOT NULL, reported smallint NOT NULL, helpful smallint NOT NULL);`, (err, res) => {
+  if (err) {
+    console.log('error creating questions table', err);
+    return;
   }
-  client.end;
+  console.log('questions table created!');
+  client.query(`CREATE TABLE IF NOT EXISTS answers ( answer_id int PRIMARY KEY NOT NULL, question_id int not null, body text not null, date_written bigint not null, answerer_name varchar(40) not null, answerer_email text not null, reported smallint, helpful smallint, CONSTRAINT fk_questions FOREIGN KEY(question_id) REFERENCES questions(question_id));`, (err, res) => {
+    if (err) {
+      console.log('error creating answers table');
+      return;
+    }
+    console.log('answers table created!');
+    client.query(`CREATE TABLE IF NOT EXISTS photos ( photo_id int PRIMARY KEY, answer_id int NOT NULL, url text NOT NULL, CONSTRAINT photos_fk FOREIGN KEY(photo_id) references answers(answer_id));`, (err, res) => {
+      if (err) {
+        console.log('error creating photos table');
+        return;
+      }
+      console.log('photos table created!');
+      // client.query('SELECT ')
+    });
+  });
 });
+
+
+module.exports = client;
